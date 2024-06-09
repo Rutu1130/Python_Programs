0ba3470b2a84e122ec5cf9f9376c70ddb7d6984a
Rutu1130 <154366245+Rutu1130@users.noreply.github.com>
2024-06-09 16:43:51 +0530

    Add files via upload

---
+++ List6.py
@@ -0,0 +1,18 @@
+
+def main():
+    print("Enter the number of elements that you want to store : ")
+    Length = int(input())
+
+    Arr = list()
+
+    print("Enter the Elements : ")
+    for i in range(Length):
+        Value = int(input())
+        Arr.append(Value)
+
+    print("Elements from the list are : ")
+    for i in range(Length):
+        print(Arr[i])
+
+if __name__ == "__main__":
+    main()
