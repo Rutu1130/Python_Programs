c99db0cd37be4ae32950f18c9d081e9fe908ca95
Rutu1130 <154366245+Rutu1130@users.noreply.github.com>
2024-06-14 20:16:10 +0530

    Add files via upload

--- Multi1.py
+++ Multi1.py
@@ -1,8 +1,22 @@
 
 import multiprocessing
+import os
+
+def Task1():
+    print("Executing the first Task...")
+    print("PID of ruuning process for task1  : ",os.getpid())
+
+
+def Task2():
+    print("Executing the first Task...")
+    print("PID of ruuning process for task2  : ",os.getpid())
 
 def main():
-    print("Number of cores are : ",multiprocessing.cpu_count())
+    print("Demonstration of multiprocessing")
+    print("PID of ruuning process is : ",os.getpid())
+
+    Task1()
+    Task2()
 
 if __name__ == "__main__":
     main()
