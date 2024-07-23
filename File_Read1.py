cfc2a3764b6b8cbb4c6add1f68bd2897ae88f2b2
Rutu1130 <154366245+Rutu1130@users.noreply.github.com>
2024-07-23 20:59:05 +0530

    Add files via upload

---
+++ File_Read1.py
@@ -0,0 +1,24 @@
+import os
+
+def main():
+    print("Enter the name of file that you want to open for Reading purpose : ")
+    File_name = input()
+
+    if os.path.exists(File_name):
+        fobj = open(File_name, "r") # read mode
+        if fobj :
+            print("File Successfully opened in the read mode....!")
+
+            print("Enter the data that you want to write in the file :")
+            Data = fobj.read(10)
+            print("Data from file is : ",Data)
+
+            fobj.close()
+        else:
+            print("Unable to open file...")
+        
+    else: 
+        print("There is no such file...")
+    
+if __name__ == "__main__":
+    main()
---
+++ File_ReadLine.py
@@ -0,0 +1,28 @@
+import os
+
+def main():
+    print("Enter the name of file that you want to open for Reading purpose : ")
+    File_name = input()
+
+    if os.path.exists(File_name):
+        fobj = open(File_name, "r") # read mode
+        if fobj :
+            print("File Successfully opened in the read mode....!")
+
+            Line1 = fobj.readline()
+            Line2 = fobj.readline()
+            Line3 = fobj.readline()
+            
+            print("First line is : ",Line1)
+            print("Second line is : ",Line2)
+            print("Third line is : ",Line3)
+
+            fobj.close()
+        else:
+            print("Unable to open file...")
+        
+    else: 
+        print("There is no such file...")
+    
+if __name__ == "__main__":
+    main()
